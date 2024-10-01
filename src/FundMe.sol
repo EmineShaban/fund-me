7e9a52642fd9cb454182539841455c6f92dcc999
Emine Shaban <emineshaban@icloud.com>
2024-10-01 13:48:23 +0300

    add chainlink imports

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.27;
 
 import {PriceConverter} from "./PriceConverter.sol";
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 error NotOwner();
 
